778770e89775d8e3b7df447226cf8e6e43b5e9c1
Clint171 <clintsimiyu004@gmail.com>
2023-10-01 12:12:32 +0300

    ui and file system changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-<h1>Clint Gretty Lionel Simiyu</h1>
-<p><a href="clint-simiyu.onrender.com">This</a> is my portfolio site</p>
+# Clint Gretty Lionel Simiyu
+<a href="clint-simiyu.onrender.com">This</a> is my portfolio site
---
+++ resources/js/portfolio.js
@@ -0,0 +1,32 @@
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = ()=>{stick()};
+
+// Get the navbar
+var navbar = document.getElementById("topnavbar");
+
+
+// Show or hide the navbar
+function showNav() {
+
+  if (navbar.classList.contains("hidden")) {
+    navbar.classList.remove("hidden");
+  } else {
+    navbar.classList.add("hidden");
+  }
+}
+
+function change(id){
+  var activeSpans = document.getElementsByClassName("active");
+  for (i in activeSpans){
+    try{
+      activeSpans[i].classList.remove("active");
+    }
+    catch(err){}
+  }
+  document.getElementById(id).classList.add("active");
+}
+
+window.onload = ()=>{
+  change("summary");
+}
