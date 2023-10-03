4f24f8be140e51d1cbceeba6a4971b5d6a5b7718
Clint171 <clintsimiyu004@gmail.com>
2023-10-03 11:49:04 +0300

    Added projects and fixed popup button

--- resources/js/portfolio.js
+++ resources/js/portfolio.js
@@ -19,7 +19,7 @@ function showNav() {
 function flashContacts(){
   var contacts = document.getElementById("footer");
   if (contacts.style.opacity > 0){
-    contacts.style.opacity = 0;
+    contacts.style.opacity = 0; 
     setTimeout(()=>{  
     contacts.style.display = "none";
     },500);
@@ -56,3 +56,35 @@ function change(id){
 window.onload = ()=>{
   change("summary");
 }
+
+//Query github api to get repositories and for each repository create a card in the projects section
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.github.com/users/Clint171/repos", true);
+xhr.onload = function(){
+  var repos = JSON.parse(this.responseText);
+  var projects = document.getElementById("projects_div");
+  for (i in repos){
+    var repo = repos[i];
+    var card = document.createElement("div");
+    card.classList.add("card");
+    var cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    var cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.innerHTML = repo.name;
+    var cardText = document.createElement("p");
+    cardText.classList.add("card-text");
+    cardText.innerHTML = repo.description;
+    var cardLink = document.createElement("a");
+    cardLink.classList.add("card-link");
+    cardLink.innerHTML = "View on Github";
+    cardLink.href = repo.html_url;
+    cardLink.target = "_blank";
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(cardLink);
+    card.appendChild(cardBody);
+    projects.appendChild(card);
+  }
+}
+xhr.send();
